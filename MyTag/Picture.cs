8eab50cfe4edc4f5d469e9bf81290679ae7a3a68
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-11-28 17:24:09 +0100

    Adding Constructor in Picture class

--- MyTag/Picture.cs
+++ MyTag/Picture.cs
@@ -2,6 +2,18 @@
 {
     public class Picture
     {
-        
+        private static string fileLocation = @"C:\MyTag";
+        private string _name;
+        private bool isInDB;
+
+        public Picture(string name, bool isInDb = true)
+        {
+            this._name = name;
+
+            if (!isInDb)
+            {
+                //call copy file to fileLocation method
+            }
+        }
     }
 }
