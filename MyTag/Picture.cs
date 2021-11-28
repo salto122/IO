750f0176a4119f2ba99ce31edef8490023ffd9a2
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-11-28 17:41:31 +0100

    Creating copy picture method Issue #17

--- MyTag/Picture.cs
+++ MyTag/Picture.cs
@@ -1,23 +1,31 @@
-﻿namespace MyTag
+﻿using System.IO;
+
+namespace MyTag
 {
     public class Picture
     {
-        private static string fileLocation = @"C:\MyTag";
-        private DB db;
+        private static string fileLocation = @"C:\MyTag\";
+        private DB _db;
         private string _name;
+        private string _dbName;
+        private string _currentPath;
         private bool isInDB;
 
         public Picture(string name, bool isInDb = true)
         {
             this._name = name;
-            db = new DB();
+            _db = new DB();
 
             if (!isInDb)
             {
-                
+                CopyPicture();
             }
         }
 
-
+        private void CopyPicture()
+        {
+            this._dbName = _db.addFile(_name);
+            File.Copy(_currentPath, fileLocation + this._dbName);
+        }
     }
 }
