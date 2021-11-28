13c5e8c5e65fc1f562fae54099d4433734d5f1c1
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-11-28 17:30:54 +0100

    Adding DB class initialization

--- MyTag/Picture.cs
+++ MyTag/Picture.cs
@@ -3,17 +3,21 @@
     public class Picture
     {
         private static string fileLocation = @"C:\MyTag";
+        private DB db;
         private string _name;
         private bool isInDB;
 
         public Picture(string name, bool isInDb = true)
         {
             this._name = name;
+            db = new DB();
 
             if (!isInDb)
             {
-                //call copy file to fileLocation method
+                
             }
         }
+
+
     }
 }
