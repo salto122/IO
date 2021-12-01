8fbc6bc73d38fb17246fd3a497ba778a864aef65
szpaw <sp055938@student.ath.edu.pl>
2021-12-01 18:56:27 +0100

    Added MongoConnection.cs

---
+++ MyTag/Database/MongoConnection.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTag.Database
+{
+    public class MongoConnection
+    {
+        IMongoDatabase db;
+        public MongoConnection()
+        {
+            
+        }
+    }
+}
