e22fba171ac4c16d510a713f4d560c2781ea981b
szpaw <sp055938@student.ath.edu.pl>
2021-12-01 19:49:48 +0100

    InsertOneUser

---
+++ MyTag/Database/Models/PictureModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyTag.Database
+{
+    public class PictureModel
+    {
+        [BsonId]
+        public Guid guid { get; set; }
+        public string OriginalName { get; set; }
+        public DateTime AddedDate { get; set; }
+        public string Tags { get; set; }
+        public string ExclTags { get; set; }
+    }
+}
