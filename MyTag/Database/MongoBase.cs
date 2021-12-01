4cf737fc3a93081ef635cea4d7aeefc43acf7bfb
szpaw <sp055938@student.ath.edu.pl>
2021-12-01 19:09:37 +0100

    InsertOne

--- MyTag/Database/MongoBase.cs
+++ MyTag/Database/MongoBase.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,19 @@ namespace MyTag.Database
         {
             var client = new MongoClient();
             db = client.GetDatabase(databaseName);
+        }
+
+        public void InsertOne<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadAll<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            return collection.Find(new BsonDocument()).ToList();
         }
     }
 }
