0179a054b4a47996ddf531a4e9fee880553aaa04
szpaw <sp055938@student.ath.edu.pl>
2021-12-01 20:47:18 +0100

    fix ObjectId

--- MyTag/Database/Models/PictureModel.cs
+++ MyTag/Database/Models/PictureModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace MyTag.Database
@@ -6,7 +7,7 @@ namespace MyTag.Database
     public class PictureModel
     {
         [BsonId]
-        public Guid guid { get; set; }
+        public ObjectId Id { get; set; }
         public string OriginalName { get; set; }
         public DateTime AddedDate { get; set; }
         public string Tags { get; set; }
