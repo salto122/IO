3238758e67d5c84d9d469b9e3933dbf9d8bd00a1
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-12-04 14:14:02 +0100

    Correcting the new DB class name in the Picture.cs class

--- MyTag/Picture.cs
+++ MyTag/Picture.cs
@@ -1,7 +1,37 @@
-﻿namespace MyTag
+﻿using System.IO;
+using MyTag.Database;
+
+namespace MyTag
 {
     public class Picture
     {
+        private const string FileLocation = @"C:\MyTag\";
+        private MongoBase _db;
+        private readonly string _name;
+        private string _dbName;
+        private string currentPath;
         
+        public Picture()
+        {
+            _db = new MongoBase("myDb");
+        }        
+        
+        public Picture(string dbName) : this()
+        {
+            this._name = dbName;
+        }
+
+        public Picture(string name, string currentPath) : this()
+        {
+            this._name = name;
+            this.currentPath = currentPath;
+            CopyPicture();
+        }        
+        
+        private void CopyPicture()
+        {
+            this._dbName = _db.addFile(_name);
+            File.Copy(currentPath, FileLocation + this._dbName);
+        }
     }
 }
