648188c957b6037453276610745fd8174ef5e5ff
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-12-04 15:33:26 +0100

    Created method that sends new tags to the DB

--- MyTag/Tags.cs
+++ MyTag/Tags.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections.Generic;
+using MyTag.Database;
 
 namespace MyTag
 {
     public class Tags
     {
         private List<string> TagList;
+        private MongoBase _mongoBase;
 
         public Tags(List<string> tagList)
         {
             TagList = tagList;
+            _mongoBase = new MongoBase("baseName");
+        }
+
+        public void SendTagsToDB()
+        {
+            _mongoBase.SendTags(TagList);
         }
     }
 }
