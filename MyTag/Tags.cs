aca15a9de45e909c9d6c4d44cb8f6c5963a74609
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-12-04 15:28:55 +0100

    Created constructor for Tags class

--- MyTag/Tags.cs
+++ MyTag/Tags.cs
@@ -1,7 +1,14 @@
-﻿namespace MyTag
+﻿using System.Collections.Generic;
+
+namespace MyTag
 {
     public class Tags
     {
-        
+        private List<string> TagList;
+
+        public Tags(List<string> tagList)
+        {
+            TagList = tagList;
+        }
     }
 }
