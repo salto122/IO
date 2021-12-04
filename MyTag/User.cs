2d26b734e140f5d54aee500db691fe79d4c59684
Michał Kuliga <mk055917@student.ath.edu.pl>
2021-12-04 14:34:00 +0100

    Creating constructor for User class

--- MyTag/User.cs
+++ MyTag/User.cs
@@ -1,7 +1,15 @@
-﻿namespace MyTag
+﻿using System.Runtime.CompilerServices;
+using MyTag.Database;
+
+namespace MyTag
 {
     public class User
     {
-        
+        private UserModel _userModel;
+
+        public User(UserModel userModel)
+        {
+            _userModel = userModel;
+        }
     }
 }
