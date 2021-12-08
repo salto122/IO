abf0ef0bca2bff2034bce07332f36cb8577bd0f9
Marcin <mzuziak2@gmail.com>
2021-12-08 22:15:52 +0100

    Image List - ListView a small fix

--- MyTag/MainWindow.xaml.cs
+++ MyTag/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,31 +23,49 @@ namespace MyTag
     {
         public string DisplayedLogo
         {
-           get { return @"C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"; }
+            get { return @"C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"; }
         }
         public MainWindow()
         {
             InitializeComponent();
 
-            var images = GetImages();
-            if (images.Count > 0)
-                ListViewImages.ItemsSource = images;
+            var Images = new List<ImageTest>()
+                                              {
+                                                new ImageTest("#TAG1", "C:/REPOZYTORIA/IO prj/MyTag/Assets/test.jpg"),
+                                                new ImageTest("#TAG2", "C:/REPOZYTORIA/IO prj/MyTag/Assets/test.jpg"),
+                                                new ImageTest("#TAG3", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"),
+                                                new ImageTest("#TAG4", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"),
+                                                new ImageTest("#TAG5", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"),
+                                                new ImageTest("#TAG6", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"),
+                                                new ImageTest("#TAG7", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"),
+                                                new ImageTest("#TAG8", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg"),
+                                                new ImageTest("#TAG9", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg")
+                                              };
+            //if (images.Count > 0)
+            //   ListViewImages.ItemsSource = images;
+            for (int i = 0; i < Images.Count; i++)
+            {
+                ListViewImages.Items.Add(new ImageTest($"#TAG{i}", "C:/REPOZYTORIA/IO prj/MyTag/Assets/test.jpg"));
+            }
+            //foreach (var item in Images)
+            //{
+            //    ListViewImages.Items.Add(new ImageTest("#TAG123", "C:/REPOZYTORIA/IO prj/MyTag/Assets/test.jpg"));
+            //}
+            var selItem = ListViewImages.SelectedItem;
 
-           
+            if (selItem != null)
+            {
+                var TEST = (ImageTest)selItem;
+                TB_TagList.Text = TEST.Tag.ToString();
+            }
+
         }
-
-        private List<ImageTest> GetImages()
+        private void LB_SelImgae(object sender, SelectionChangedEventArgs e)
         {
-            return new List<ImageTest>()
-      {
-        new ImageTest("#TAG1", "C:/REPOZYTORIA/IO prj/MyTag/Assets/test.jpg"),
-        new ImageTest("#TAG2", "C:/REPOZYTORIA/IO prj/MyTag/Assets/test.jpg"),
-        new ImageTest("#TAG2", "C:/REPOZYTORIA/IO prj/MyTag/Assets/logo.jpg")
-
-        
-
-      };
+            var item = (ListBox)sender;
+           TB_TagList.Text= ((ImageTest)item.SelectedItem).Tag;
         }
+
 
         private void BT_ShowMyTags_CLick(object sender, RoutedEventArgs e)
         {
@@ -87,24 +106,22 @@ namespace MyTag
         {
 
         }
-        private void BT_Image_Click(object sender, RoutedEventArgs e)
+
+
+
+        public partial class ImageTest
         {
+            public string Tag { get; set; }
+            public string ImagePath { get; set; }
+            public BitmapImage Image { get; set; }
 
-        }
-    }
+            public ImageTest(string tag, string imagePath)
+            {
+                Tag = tag;
+                ImagePath = imagePath;
+                Image = new BitmapImage(new Uri(imagePath, UriKind.Relative));
 
-    public partial class ImageTest
-    {
-        public string Tag { get; set; }
-        public string ImagePath { get; set; }
-        public BitmapImage Image { get; set; }
-
-        public ImageTest(string tag, string imagePath)
-        {
-            Tag = tag;
-            ImagePath = imagePath;
-            Image = new BitmapImage(new Uri(imagePath, UriKind.Relative));
-
+            }
         }
     }
 }
