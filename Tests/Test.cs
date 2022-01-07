a748d47c60a733f4c8a62902e351672c52c56654
szpaw <sp055938@student.ath.edu.pl>
2022-01-07 19:31:03 +0100

    First assert

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -1,4 +1,6 @@
 using Backend;
+using Backend.Database;
+using MongoDB.Bson;
 using NUnit.Framework;
 
 namespace Tests
@@ -6,18 +8,12 @@ namespace Tests
     [TestFixture]
     public class TestClassTest
     {
-        // private TestClass _testClass = new TestClass();
-        //
-        // [TestCase]
-        // public void DoesAddValueWork()
-        // {
-        //     Assert.AreEqual(0, _testClass.localInt);
-        //     
-        //     _testClass.AddValue(2);
-        //     Assert.AreEqual(2, _testClass.localInt);
-        //     
-        //     _testClass.AddValue(2);
-        //     Assert.AreEqual(4, _testClass.localInt);
-        // }
+        private MongoBase dbTest = new MongoBase(MongoConnection.DatabaseName);
+
+        [TestCase]
+        public void DoesAddValueWork()
+        {
+            Assert.AreEqual(new ObjectId("000000000000000000000000"), dbTest.InsertOnePicture("pictures", new Backend.Database.Models.PictureModel("empty", "")));
+        }
     }
 }
